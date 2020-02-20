b8610c5ab9cfd63601c9c1058e9bec917c9d3914
Anton Klimantov <klimantow@yandex.ru>
2020-02-20 14:36:32 +0300

    clearing project for new style

--- src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./useSelect"
-export * from "./useTimer"
-export * from "./useInput"
-export * from "./useTimeline"
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./LoginPage"
-export * from "./TasksPage"
-export * from "./ObjectPage"
-export * from "./UserSettingPage"
-export * from "./CompanyPage"
