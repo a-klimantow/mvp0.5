e2754a29cb5fdff6c240be412ac8fa0a05e2b433
Anton Klimantow <klimantow@yandex.ru>
2020-05-22 13:42:02 +0300

    test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { App } from "components"
 import * as serviceWorker from "./serviceWorker"
 import { BrowserRouter } from "react-router-dom"
 
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
