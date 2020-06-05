bc45570a3a0748a66123e5d75d1b6e615e9054b2
Anton Klimantov <klimantow@yandex.ru>
2020-06-05 15:10:10 +0300

    create menu

--- src/01/pages/LoginPage.js
+++ src/01/pages/LoginPage.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { useRouteMatch, Route } from "react-router-dom"
+import { Route } from "react-router-dom"
 import styled, { css } from "reshadow/macro"
 
-import { useLoginPage } from "./useLoginPage"
+import { useLoginPage } from "01/hooks/useLoginPage"
 
 export const LoginPage = ({ style }) => {
   const { email, password, btn, form } = useLoginPage()
---
+++ src/01/pages/Tasks.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Route, Redirect, Switch } from "react-router-dom"
+
+import { useTasks } from "01/hooks/useTasks"
+import { Tabs } from "01/components/Tabs"
+
+export const Tasks = () => {
+  return <>tasks</>
+}
